da6ed0db9fb57fc9f19a2433b56a02878d594e72
Debora Blass <debbieblass@gmail.com>
2014-01-14 15:54:17 -0500

    add ruby flash

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "http://rubygems.org"
 
 # The Web microframework.
 gem "sinatra"
+gem "sinatra-flash"
--- veggies.rb
+++ veggies.rb
@@ -1,10 +1,21 @@
 require 'sinatra'
+require 'sinatra/flash'
+enable :sessions
+
+
 
 get '/' do
-  @guesses = 8
   erb :index
 end
 
-get '/check' do
-  "Hello World"
+post '/' do
+  @guess = params[:guess]
+  @words = ['Carrots', 'Potatoes', 'Sellery', 'Eggplants', 'Spinage', 'Bellpeppers']
+  if @words.include?('@guess')
+    flash[:info] = "Your guess (#{@guess}) was correct"
+  else
+    flash[:info] = "Your guess (#{@guess}) was not correct"
+  end
+
+  redirect '/'
 end
