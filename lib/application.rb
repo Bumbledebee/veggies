718ed54c85a05398c6571a9ee67749c00edbee76
Debora Blass <debbieblass@gmail.com>
2014-01-19 18:28:12 -0500

    chinese year

--- lib/application.rb
+++ lib/application.rb
@@ -3,25 +3,34 @@
 class Application
   def initialize(year)
     @year = year
-    @zodiac = ''
   end
 
   def chinese_year
-    case @zodiac
-      when (@year%12) == 0 then "Monkey"
-      when (@year%12) == 1 then "Rooster"
-      when (@year%12) == 2 then "Dog"
-      when (@year%12) == 3 then "Pig"
-      when (@year%12) == 4 then "Rat"
-      when (@year%12) == 5 then "Ox"
-      when (@year%12) == 6 then "Tiger"
-      when (@year%12) == 7 then "Rabbit"
-      when (@year%12) == 8 then "Dragon"
-      when (@year%12) == 9 then "Snake"
-      when (@year%12) == 10 then "Horse"
-      when (@year%12) == 11 then "Goat"
+    if @year%12 == 0
+      zodiac = "Monkey"
+    elsif @year%12 == 1
+      zodiac = "Rooster"
+    elsif @year%12 == 2
+      zodiac = "Dog"
+    elsif @year%12 == 3
+      zodiac = "Pig"
+    elsif @year%12 == 4
+      zodiac = "Rat"
+    elsif @year%12 == 5
+      zodiac = "Ox"
+    elsif @year%12 == 6
+      zodiac = "Tiger"
+    elsif @year%12 == 7
+      zodiac = "Rabbit"
+    elsif @year%12 == 8
+      zodiac = "Dragon"
+    elsif @year%12 == 9
+      zodiac = "Snake"
+    elsif @year%12 == 10
+      zodiac = "Horse"
+    elsif @year%12 == 11
+      zodiac = "Goat"
     end
-    @zodiac
   end
 
 end
